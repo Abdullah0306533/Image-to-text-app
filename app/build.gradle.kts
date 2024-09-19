5b449823e14b8ef36605078a9c2bf27a8aee3230
abdullah-butt <abdullah0306533@gmail.com>
2024-09-19 10:57:33 +0500

    created the first layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -40,4 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("com.google.mlkit:text-recognition:16.0.1")
 }
